a4d07f6b983301ddffcc6900c90ed370260eefb1
albarsun <56282764+albarsun@users.noreply.github.com>
2019-12-14 02:21:08 +0530

    Create relationship_status.c

---
+++ relationship_status.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+
+{
+  printf("I had a girlfriend and she was cute");
+  printf(" and one of a kind.\n");
+  printf("We have broken up now because long distance relationship does not work.\n");
+}
